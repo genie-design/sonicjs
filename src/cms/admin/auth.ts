8703726b58ce259db1d5464954a49f2dacd03e43
Corey Jepperson <corey.jepperson@gmail.com>
2024-01-06 22:46:11 -0600

    remove err

--- src/cms/admin/auth.ts
+++ src/cms/admin/auth.ts
@@ -49,7 +49,6 @@ const fieldsAccess = userTableConfig?.access?.fields;
 // View user
 authAPI.get(`/users/:id`, async (ctx) => {
   const id = ctx.req.param("id");
-  console.error("sentry test", id);
   if (userTableConfig.hooks?.beforeOperation) {
     await userTableConfig.hooks?.beforeOperation(ctx, "read", id);
   }
