fb4d8ae214de09f8e4f0b320b61603cd538c2959
Corey Jepperson <corey.jepperson@gmail.com>
2024-04-06 17:33:38 -0500

    prettier

--- src/cms/admin/pages/content.tsx
+++ src/cms/admin/pages/content.tsx
@@ -285,33 +285,33 @@ function editScript() {
 export const RelationModal = () => {
   return (
     <div
-      class="modal fade"
-      id="relationModal"
+      class='modal fade'
+      id='relationModal'
       tabindex={-1}
-      aria-labelledby="relationModalLabel"
-      aria-hidden="true"
+      aria-labelledby='relationModalLabel'
+      aria-hidden='true'
     >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="relationModalLabel">
+      <div class='modal-dialog'>
+        <div class='modal-content'>
+          <div class='modal-header'>
+            <h1 class='modal-title fs-5' id='relationModalLabel'>
               Relation Modal
             </h1>
             <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
+              type='button'
+              class='btn-close'
+              data-bs-dismiss='modal'
+              aria-label='Close'
             ></button>
           </div>
-          <div class="modal-body">
-            <div id="relationGrid"></div>
+          <div class='modal-body'>
+            <div id='relationGrid'></div>
           </div>
-          <div class="modal-footer">
+          <div class='modal-footer'>
             <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
+              type='button'
+              class='btn btn-secondary'
+              data-bs-dismiss='modal'
             >
               Close
             </button>
