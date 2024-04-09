1c0ac25fe6fe11a45d712ece753d7dac15575bd5
Corey Jepperson <corey.jepperson@gmail.com>
2024-04-08 21:47:23 -0500

    graphql working

--- src/cms/admin/graphql.ts
+++ src/cms/admin/graphql.ts
@@ -1,24 +1,27 @@
 import { Hono } from 'hono';
 import { Bindings } from '../types/bindings';
-const yoga = createYoga({
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      type Query {
-        hello: String!
-      }
-    `,
-    resolvers: {
-      Query: {
-        hello: () => 'Hello World!'
-      }
-    }
-  })
-});
 import { Variables } from '../../server';
-import { createSchema, createYoga } from 'graphql-yoga';
+
+import { SQLiteTable } from 'drizzle-orm/sqlite-core';
+import { schema as dbSchema } from '../../db/routes';
+import { drizzle } from 'drizzle-orm/d1';
+import { createYoga } from 'graphql-yoga';
+import { buildVanillaSchema } from 'drizzle-graphql';
+import { is } from 'drizzle-orm/entity';
 const graphqlAPI = new Hono<{ Bindings: Bindings; Variables: Variables }>();
-graphqlAPI.use('*', async (context) => {
+graphqlAPI.use('*', async (ctx) => {
+  console.time('create yoga');
+  const db = drizzle(ctx.env.D1DATA, dbSchema);
+  const schemaEntries = Object.entries(dbSchema);
+  const tables = Object.fromEntries(
+    schemaEntries.filter(([key, value]) => is(value, SQLiteTable))
+  );
+  const { schema } = buildVanillaSchema(db, dbSchema.schema);
+  const yoga = createYoga({ schema, logging: 'debug' });
+  console.timeEnd('create yoga');
   // @ts-ignore
-  return yoga.handle(context.req, {});
+  console.log('HANDLE');
+  return yoga.handle(ctx.req, {});
 });
+
 export { graphqlAPI };
