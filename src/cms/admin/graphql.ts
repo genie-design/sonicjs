ac738564a7619d6cf1e5873f2b3ea9ca122c9083
Corey Jepperson <corey.jepperson@gmail.com>
2024-04-09 11:04:26 -0500

    remove mutations

--- src/cms/admin/graphql.ts
+++ src/cms/admin/graphql.ts
@@ -1,6 +1,12 @@
 import { Hono } from 'hono';
 import { Bindings } from '../types/bindings';
 import { Variables } from '../../server';
+import {
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema
+} from 'graphql';
 
 import { SQLiteTable } from 'drizzle-orm/sqlite-core';
 import { schema as dbSchema } from '../../db/routes';
@@ -10,15 +16,28 @@ import { buildVanillaSchema } from 'drizzle-graphql';
 import { is } from 'drizzle-orm/entity';
 const graphqlAPI = new Hono<{ Bindings: Bindings; Variables: Variables }>();
 graphqlAPI.use('*', async (ctx) => {
-  console.time('create yoga');
   const db = drizzle(ctx.env.D1DATA, dbSchema);
   const schemaEntries = Object.entries(dbSchema);
   const tables = Object.fromEntries(
     schemaEntries.filter(([key, value]) => is(value, SQLiteTable))
   );
-  const { schema } = buildVanillaSchema(db, dbSchema.schema);
+  const { entities } = buildVanillaSchema(db, dbSchema.schema);
+  delete entities.queries.userkeys;
+  delete entities.queries.userkeysSingle;
+  delete entities.queries.usersessions;
+  delete entities.queries.usersessionsSingle;
+  delete entities.queries.users;
+  delete entities.queries.usersSingle;
+  const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'Query',
+      fields: {
+        ...entities.queries
+      }
+    })
+  });
+
   const yoga = createYoga({ schema, logging: 'debug' });
-  console.timeEnd('create yoga');
   // @ts-ignore
   console.log('HANDLE');
   return yoga.handle(ctx.req, {});
